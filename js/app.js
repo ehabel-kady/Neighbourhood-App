f1608431d083807405ca40abfd40777992c56b53
Ehab <ehabadelkady@outlook.com>
2018-07-13 05:23:06 +0200

    Worked with some errors in filtration

--- js/app.js
+++ js/app.js
@@ -9,13 +9,41 @@ var locations =
 {title: 'Fahmy Kebda',location: {lat:29.973445 ,lng: 30.944820} },
 {title: 'chick chicken',location: {lat: 29.969233,lng: 30.940526} },
 ];
-
+var eleminated = [];
 function ViewModel()
 {
     var self=this
-    var positions = ko.observableArray(locations)
-    var markers=[];
-    var largeInfowindow = new google.maps.InfoWindow();
+    this.positions = ko.observableArray(locations)
+    this.markers = [];
+    this.searchTerm = ko.observable('');
+    // this.largeInfowindow = new google.maps.InfoWindow();
+    this.filtration = function()
+    {
+        this.showlisting();
+        this.positions([]);
+        eleminated=[];
+        var searchfilter = this.searchTerm().toLowerCase();
+        for(var i =0; i<locations.length;i++)
+        {
+            if(locations[i].title.toLowerCase().indexOf(searchfilter) != -1)
+            {
+                this.positions.push(locations[i]);
+            }
+            else{
+                eleminated.push(locations[i]);
+            }
+        }
+        for(var i = 0; i < this.markers.length;i++)
+        {
+            for(var j = 0; j < eleminated.length;j++)
+            {
+                if(self.markers[i].title == eleminated[i].title)
+                {
+                    this.markers[i].setMap(null);
+                }
+            }
+        }
+    };
     this.populateInfoWindow=function(marker,infowindow){
         if(infowindow.marker != marker){
             infowindow.marker = marker;
@@ -25,27 +53,61 @@ function ViewModel()
                 infowindow.marker = null;
             });
         }
-    }
-    this.bounceEffect = function () {
-        self.populateInfoWindow(this, self.largeInfoWindow);
-        this.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout((function () {
-            this.setAnimation(null);
-        }).bind(this), 600);
     };
+    this.viewlist=function()
+    {
+        for(var i = 0; i<self.markers.length;i++)
+        {
+            if(self.markers[i].title == event.title)
+            {
+                this.marker = self.markers[i];
+                break;
+            }
+        }
+        self.populateInfoWindow(this.marker,self.largeInfowindow);
+        this.marker.setAnimation(google.maps.Animation.DROP);
+        setTimeout((function(){
+            this.marker.setAnimation(null);
+        }).bind(this),700);
+    };
+    this.showlisting = function()
+    {
+        this.bounds = new google.maps.LatLngBounds();
+        for(var i = 0; i < this.markers.length;i++)
+        {
+            this.markers[i].setMap(map);
+            this.bounds.extend(this.markers[i].position);
+        }
+        map.fitBounds(this.bounds);
+    };
+    this.hidelisting = function()
+    {
+        for(var i = 0; i < this.markers.length;i++)
+        {
+            for(var j=0; j<eleminated.length;j++)
+            {
+                this.markers[i].setMap(null);
+            }
+        }
+    };
+
     this.initmap=function()
     {
+        this.largeInfoWindow = new google.maps.InfoWindow();
         map=new google.maps.Map(document.getElementById('map'),{
             center: {lat: 30.012031,lng: 30.987625},
             zoom: 13,
             mapTypeControl: false
         });
         // setmarkers();
-        for(var i = 0; i < positions().length;i++)
+        function clickTopopulate() {
+            self.populateInfoWindow(this, self.largeInfoWindow);
+          }
+        for(var i = 0; i < locations.length;i++)
         {
-            var position = positions()[i].location;
-            var title = positions()[i].title;
-            var marker = new google.maps.Marker({
+            var position = locations[i].location;
+            var title = locations[i].title;
+            this.marker = new google.maps.Marker({
                 map: map,
                 position:position,
                 title:title,
@@ -53,65 +115,22 @@ function ViewModel()
                 id: i
             });
             this.markers.push(this.marker);
-            this.marker.addListener('click',function(){
-                self.populateInfoWindow(this,largeInfowindow);
+            this.marker.addListener('click', function() {
+                this.setAnimation(4);
             });
+            // add an infowindow populator function
+            this.marker.addListener('click', clickTopopulate);
         }
-        var bounds = new google.maps.LatLngBounds();
-        for(var i = 0; i < markers.length;i++)
-        {
-            markers[i].setMap(map);
-            bounds.extend(markers[i].location);
-        }
-        map.fitBounds(bounds);
+        this.showlisting();
     };
     this.initmap();
+
+    this.toggle = function()
+    {
+        var wrap = documen
+    }
 };
 function runapp()
 {
     ko.applyBindings(new ViewModel());
 }
-// var markers = [];
-// function initMap()
-// {
-//     new google.maps.Map(document.getElementById('map'),{
-//         center: {lat: 30.012031,lng: 30.987625},
-//         zoom: 13,
-//         mapTypeControl: false
-//     });
-//     var largeInfowindow= new google.maps.InfoWindow();
-
-//     for(var i = 0; i<locations.length;i++)
-//     {
-//         var position=locations[i].location;
-//         var title=locations[i].title;
-//         var marker=new google.maps.Marker({
-//             position: position,
-//             title: title,
-//             animation: google.maps.Animation.DROP,
-//             id: i
-//         });
-//         markers.push(marker);
-//         marker.addListener('click', function() {
-//             populateInfoWindow(this, largeInfowindow);
-//         });
-//     }
-//     var bounds = new google.maps.LatLngBounds();
-//     // Extend the boundaries of the map for each marker and display the marker
-//     for (var i = 0; i < markers.length; i++) {
-//       markers[i].setMap(map);
-//       bounds.extend(markers[i].position);
-//     }
-//     // map.fitBounds(bounds);
-// };
-// function populateInfoWindow(marker, infowindow)
-// {
-//     if(infowindow.marker != marker){
-//         infowindow.marker = marker;
-//         infowindow.setContent('<div>'+marker.title+'</div>');
-//         infowindow.open(map,marker);
-//         infowindow.addListener('closeclick', function() {
-//             infowindow.marker = null;
-//         });
-//     }
-// };
