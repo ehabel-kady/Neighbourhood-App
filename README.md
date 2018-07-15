d15a07b12988bffee511b908399ddd286c5e46a3
Ehab <ehabadelkady@outlook.com>
2018-07-15 03:04:35 +0200

    updated the markers

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Neighbourhood-App
+This a web application that displays lots of locations on google maps. When clicking on the window it displays a message with more information about the location.
+## Used technologies:
+To build this app the foloowing is using:
+-javascript knockout library
+-Ajax
+-Google maps APIs
+### What I learned:
+-Using knockout library
+-Using google maps APIs
--- js/app.js
+++ js/app.js
@@ -1,136 +1,176 @@
+// var locations = [
+//     {name: 'Misr University for science and technology',location: {lat: 29.998875,lng: 30.965052} },
+//     {name: 'Mall of Arabia',location: {lat: 30.006308,lng: 30.973463} },
+//     {name: 'Koshary Eltahrir',location: {lat: 29.973598,lng: 30.945799} },
+//     {name: 'Zacks',location: {lat: 29.958662,lng: 30.917630} },
+//     {name: 'Alnassagoun alSharqeyoon',location: {lat: 29.954377  ,lng: 30.913789} },
+//     {name: 'Fahmy Kebda',location: {lat:29.973445 ,lng: 30.944820} },
+//     {name: 'chick chicken',location: {lat: 29.969233,lng: 30.940526} },
+// ];
 var map;
-var locations =
-[
-{title: 'Misr University for science and technology',location: {lat: 29.998875,lng: 30.965052} },
-{title: 'Mall of Arabia',location: {lat: 30.006308,lng: 30.973463} },
-{title: 'Koshary Eltahrir',location: {lat: 29.973598,lng: 30.945799} },
-{title: 'Zacks',location: {lat: 29.958662,lng: 30.917630} },
-{title: 'Alnassagoun alSharqeyoon',location: {lat: 29.954377  ,lng: 30.913789} },
-{title: 'Fahmy Kebda',location: {lat:29.973445 ,lng: 30.944820} },
-{title: 'chick chicken',location: {lat: 29.969233,lng: 30.940526} },
+var places = [
+    {name: 'Misr University for science and technology',coord: {lat: 29.998875,lng: 30.965052}},
+    {name: 'Mall of Arabia', coord: {lat: 30.006308,lng: 30.973463}},
+    {name: 'Koshary Eltahrir', coord: {lat: 29.973598,lng: 30.945799}},
+    {name: 'Zacks', coord: {lat: 29.958662,lng: 30.917630}},
+    {name: 'Fahmy Kebda', coord: {lat:29.973445 ,lng: 30.944820}},
+    {name: 'chick chicken', coord: {lat: 29.969233,lng: 30.940526}}
 ];
-var eleminated = [];
-function ViewModel()
+
+function viewModel()
 {
-    var self=this
-    this.positions = ko.observableArray(locations)
-    this.markers = [];
+    var self = this;
     this.searchTerm = ko.observable('');
-    // this.largeInfowindow = new google.maps.InfoWindow();
-    this.filtration = function()
+    self.positions = ko.observableArray(places);
+    this.listinfowindow = new google.maps.InfoWindow();
+    this.markers = [];
+
+    this.listview = function()
     {
-        this.showlisting();
-        this.positions([]);
-        eleminated=[];
-        var searchfilter = this.searchTerm().toLowerCase();
-        for(var i =0; i<locations.length;i++)
+        self.positions([]);
+        if(this.searchTerm() === "")
         {
-            if(locations[i].title.toLowerCase().indexOf(searchfilter) != -1)
+            for(var i = 0; i < places.length; i++)
             {
-                this.positions.push(locations[i]);
-            }
-            else{
-                eleminated.push(locations[i]);
+                self.positions.push(places[i]);
             }
         }
-        for(var i = 0; i < this.markers.length;i++)
+        else
         {
-            for(var j = 0; j < eleminated.length;j++)
+            for(var j = 0; j < places.length; j++)
             {
-                if(self.markers[i].title == eleminated[i].title)
+                if(places[i].name.toLocaleLowerCase().includes(this.searchTerm()))
                 {
-                    this.markers[i].setMap(null);
+                    self.positions.push(places[j]);
+                }
+            }
+        }
+
+        for(var i = 0; i < this.markers.length; i++)
+        {
+            this.markers[i].mapsMarker.setMap(null)
+        }
+        for (var k =0; k<this.markers.length;k++){
+            for (var l =0; l<this.positions().length;l++){
+                if (this.positions()[l].name == this.markers[k].name){
+                    this.markers[k].mapsMarker.setMap(map);
+                    this.markers[k].mapsMarker.setAnimation(4);
                 }
             }
         }
     };
-    this.populateInfoWindow=function(marker,infowindow){
-        if(infowindow.marker != marker){
-            infowindow.marker = marker;
-            infowindow.setContent('<div>'+marker.title+'</div>');
-            infowindow.open(map,marker);
-            infowindow.addListener('closeclick', function() {
-                infowindow.marker = null;
-            });
-        }
-    };
-    this.viewlist=function()
-    {
-        for(var i = 0; i<self.markers.length;i++)
-        {
-            if(self.markers[i].title == event.title)
-            {
-                this.marker = self.markers[i];
-                break;
-            }
-        }
-        self.populateInfoWindow(this.marker,self.largeInfowindow);
-        this.marker.setAnimation(google.maps.Animation.DROP);
-        setTimeout((function(){
-            this.marker.setAnimation(null);
-        }).bind(this),700);
-    };
-    this.showlisting = function()
-    {
-        this.bounds = new google.maps.LatLngBounds();
-        for(var i = 0; i < this.markers.length;i++)
-        {
-            this.markers[i].setMap(map);
-            this.bounds.extend(this.markers[i].position);
-        }
-        map.fitBounds(this.bounds);
-    };
-    this.hidelisting = function()
-    {
-        for(var i = 0; i < this.markers.length;i++)
-        {
-            for(var j=0; j<eleminated.length;j++)
-            {
-                this.markers[i].setMap(null);
-            }
-        }
-    };
 
-    this.initmap=function()
+    this.initMap = function()
     {
-        this.largeInfoWindow = new google.maps.InfoWindow();
-        map=new google.maps.Map(document.getElementById('map'),{
-            center: {lat: 30.012031,lng: 30.987625},
+        this.infomarker = new google.maps.InfoWindow();
+        map = new google.maps.Map(document.getElementById('map'),{
+            center: {lat: 30.012031, lng: 30.987625},
             zoom: 13,
             mapTypeControl: false
         });
-        // setmarkers();
-        function clickTopopulate() {
-            self.populateInfoWindow(this, self.largeInfoWindow);
-          }
-        for(var i = 0; i < locations.length;i++)
+        function initinfowindow()
         {
-            var position = locations[i].location;
-            var title = locations[i].title;
+            self.getInfo(this,self.infomarker);
+        }
+        for(var i = 0; i < places.length; i++)
+        {
             this.marker = new google.maps.Marker({
-                map: map,
-                position:position,
-                title:title,
+                name: places[i].name,
+                position: places[i].coord,
                 animation: google.maps.Animation.DROP,
-                id: i
+                map:map
             });
-            this.markers.push(this.marker);
-            this.marker.addListener('click', function() {
+            this.marker.addListener('click', function(){
                 this.setAnimation(4);
             });
-            // add an infowindow populator function
-            this.marker.addListener('click', clickTopopulate);
+            this.marker.addListener('click',initinfowindow);
+            this.markers.push({mapsMarker: this.marker, name: places[i].name});
         }
-        this.showlisting();
     };
-    this.initmap();
+    this.initMap();
 
-    this.toggle = function()
+    this.goToMarker = function(selected)
     {
-        var wrap = documen
-    }
+        for(var i =0; i < self.markers.length; i++)
+        {
+            if(self.marker[i].name == selected.name)
+            {
+                this.markerFromList = self.markers[i];
+            }
+        }
+        this.markerFromList.mapsMarker.setAnimation(4);
+        var url = 'https://api.foursquare.com/v2/venues/search?query=' + this.markerFromList.name.split(" ")[0] + '&ll=' + 
+        this.markerFromList.mapsMarker.position.lat() + ',' + this.markerFromList.mapsMarker.position.lng() + 
+        '&oauth_token=X0KE3R0WRFQYYSEDZBVSG32VKLJVUNC3YHXR5Z3V0GSZHFAU&v=20180312';
+        console.log(this.markerFromList);
+        $.ajax({
+            url: url,
+            markerFromList: this.markerFromList,
+            success: function(response)
+            {
+                var infoObject = response.response.venues[2];
+                if(infoObject.location.address===undefined)
+                {
+                    infoObject.location.address="6th of October";
+                }
+                if (infoObject.contact.phone===undefined){
+                    infoObject.contact.phone="112 (Phonebook Service)";
+                }
+                self.listinfowindow.setContent(
+                    '<div style="text-align:center">' +
+                    '<h1>' + this.markerFromList.name + '</h1>' +
+                    '<p> Address: ' + infoObject.location.address + '</p>' + 
+                    '<p> Phone: '  + infoObject.contact.phone + '</p>' + 
+                    '<p> Country: ' + infoObject.location.country + '</p>' + 
+                     '</div>'
+                );
+            }
+        }).fail(function(){
+            alert('Error encountered while fetching info.');
+        });
+        self.listinfowindow.open(map, this.markerFromList.mapsMarker);
+    };
+    this.getInfo = function(marker,infowindow)
+    {
+        var url = 'https://api.foursquare.com/v2/venues/search?query=' + marker.name.split(" ")[0] + '&ll=' + 
+        this.marker.position.lat() + ',' + this.marker.position.lng() + 
+        '&oauth_token=X0KE3R0WRFQYYSEDZBVSG32VKLJVUNC3YHXR5Z3V0GSZHFAU&v=20180312';
+        $.ajax({
+            url:url,
+            success: function(response){
+                // Chosen 3rd query object upon trial and error
+                var infoObject = response.response.venues[2];
+                if (infoObject.location.address===undefined){
+                    infoObject.location.address="6th of October";
+                }
+
+                if (infoObject.contact.phone===undefined){
+                    infoObject.contact.phone="112 (Phonebook Service)";
+                }
+
+                // console.log(infoObject);
+                infowindow.setContent(
+                    '<div style="text-align:center">' +
+                    '<h1>' + marker.name + '</h1>' +
+                    '<p> Address: ' + infoObject.location.address + '</p>' + 
+                    '<p> Phone: '  + infoObject.contact.phone + '</p>' + 
+                    '<p> Country: ' + infoObject.location.country + '</p>' + 
+                     '</div>'
+                );
+            }
+        }).fail(function(){
+            alert('Error encountered while fetching info.');
+        });
+
+        infowindow.open(map,marker);
+    };
 };
-function runapp()
-{
-    ko.applyBindings(new ViewModel());
+
+function handleError(){
+    alert("Error loading Google Maps object.");
+}
+
+
+function runapp() {
+    ko.applyBindings(new viewModel());
 }
